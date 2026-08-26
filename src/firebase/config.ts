bb6b19a2d1f655afdf6e49338b05218b58f18209
Milan Mostertman <milanmostertman@gmail.com>
2026-08-26 08:35:52 +0200

    Configure Firebase web app

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+import type { FirebaseOptions } from 'firebase/app';
+
+/** Public Firebase web configuration for the Dageraad project. */
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyCT1YqGOVJCS0-C1ZE6lMiIji454Iu81JE',
+  authDomain: 'dageraad-fdb2d.firebaseapp.com',
+  projectId: 'dageraad-fdb2d',
+  storageBucket: 'dageraad-fdb2d.firebasestorage.app',
+  messagingSenderId: '365821323201',
+  appId: '1:365821323201:web:dc725f63b0011e6fbb3140',
+};
